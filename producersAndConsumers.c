201c41ed60f20b285395d6d9f62f2b61707a10aa
ChristensenAlex <alex.chrste@live.com>
2018-10-30 20:12:36 -0600

    Output to file instead of screen. Programs now compiles without warnings.

--- producersAndConsumers.c
+++ producersAndConsumers.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "common_threads.h"
 #include <pthread.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
@@ -10,6 +11,7 @@
 
 // Uses coarse grained locks, optimized for a single processor. Running on multipul processors won't provide speedup, until fine-grained locks are used.
 pthread_mutex_t file_lock;
+FILE* output;
 
 //Helpers for circular array
 pthread_mutex_t circular_lock;	//Lock for entire circular array
@@ -29,10 +31,11 @@ pthread_cond_t fill, empty;
 
 // The same put function as book
 // Lock before calling this function
-void put(int value){
+void* put(int value){
 	buffer[fill_ptr] = value;
 	fill_ptr = (fill_ptr + 1) % MAX; // Next spot in a circular array
 	count++;
+	return NULL;
 }
 
 // Complimentry to put function
@@ -43,17 +46,22 @@ int get(){
 	return value;	
 }
 
-void commit(int values[], FILE* output){
+//commits to output
+//pass in an array of size one-hundred
+void* commit(int values[100]){
 	Pthread_mutex_lock(&file_lock);
-	for(int i = 0; values[i] != '\0'; i++){
+	for(int i = 0; i < 100; i++){
 		fprintf(output, "%d\n", values[i]);
 	}
 	Pthread_mutex_unlock(&file_lock);
+	fflush(output);
+	return NULL;
 }
 
 // Creates rand numbers to pipe to consumer
-void producer(){
+void* producer(){
 	for (int i = 0; TRUE; i++){
+		pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 		int value = rand()%5;//produces a number to place on queue
 		Pthread_mutex_lock(&circular_lock);
 		while (count == MAX){
@@ -62,14 +70,17 @@ void producer(){
 		put(value);
 		Pthread_cond_signal(&fill);// Must I signal before releasing the lock
 		Pthread_mutex_unlock(&circular_lock);
+		pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
 	}
+	return NULL;
 
 }
 
 // Adds numbers to running total
-void consumer(FILE* output){
+void* consumer(){
 	while(TRUE){
-		int temporary_log[100];
+		int temporary_log[100] = {0};
+		pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 		for(int i = 0; i < 100; i++){
 			//get number from queue
 			Pthread_mutex_lock(&circular_lock);
@@ -86,9 +97,12 @@ void consumer(FILE* output){
 			Pthread_mutex_unlock(&bucket_lock);
 			temporary_log[i] = value; //I print after release lock, so other processes can run.
 		}
-		commit(temporary_log, &output);
-	}
+		commit(temporary_log);
+		pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL); 
+	}		
+	return NULL;
 }
+
 
 
 
@@ -96,28 +110,29 @@ void consumer(FILE* output){
 //This spins up the producers and consumers
 //FUTURE PLAN: this will exit after total reaches a large number
 int main (int argc, char *argv[]){
-	FILE* output = fopen("output.txt", "w+");
+	output = fopen("output.txt", "w");
 	fprintf(output, "OUTPUT FILE\n");
 	pthread_t p1, p2, p3, c1, c2;
 	Pthread_create(&p1, NULL, &producer, NULL);
 	Pthread_create(&p2, NULL, &producer, NULL);
 	Pthread_create(&p3, NULL, &producer, NULL);
-	Pthread_create(&c1, NULL, &consumer, &output);
-	Pthread_create(&c2, NULL, &consumer, &output);
+	Pthread_create(&c1, NULL, &consumer, NULL);
+	Pthread_create(&c2, NULL, &consumer, NULL);
 
-	sleep(3);
+	sleep(1);
 	
-	pthread_kill(&p1);
-	pthread_kill(&p2);
-	pthread_kill(&p3);
-	pthread_kill(&c1);
-	pthread_kill(&c2);
+	pthread_cancel(p1);
+	pthread_cancel(p2);
+	pthread_cancel(p3);
+	pthread_cancel(c1);
+	pthread_cancel(c2);
 
 
 	//both producer and consumer run till killed, so joins not included.
 	for(int i = 0; i < 5; i++){
-		printf("   %d: %ld", i, buckets[i]);
+		printf("   %d: %ld\n", i, buckets[i]);
 	}
+	fclose(output);
 				
 
 }
